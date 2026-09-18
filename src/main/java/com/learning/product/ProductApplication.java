f37f7ffb400a4787c26ee9385d6efb6503bdc80d
Ankit Mavi <ankitmaavi@gmail.com>
2026-09-18 08:28:13 +0530

    Add Eureka discovery and feign client

--- src/main/java/com/learning/product/ProductApplication.java
+++ src/main/java/com/learning/product/ProductApplication.java
@@ -2,10 +2,12 @@ package com.learning.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
 @EnableSpringDataWebSupport
+@EnableFeignClients
 public class ProductApplication {
 
 	public static void main(String[] args) {
